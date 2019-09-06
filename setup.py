ee44082d1bc6a4a4c42b00e84fae7a9e91d73cbb
Readman Chiu <rchiu@bcgsc.ca>
2019-09-05 17:01:44 -0700

    setup files

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+from version.py import __version__
+setup(
+    name='straglr',
+    version=__version__,
+    description='Straglr',
+    long_description='Short tandem repeat genotyping using long reads',
+    url='https://github.com/bcgsc/straglr.git',
+    author='Readman Chiu',
+    author_email='rchiu@bcgsc.ca',
+    license='BCCA',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        ],
+    packages=find_packages(),
+    install_requires = [
+        'pysam>=0.15.0',
+        'pybedtools>=0.7.9',
+        'intspan>=1.5.8',
+        'numpy>=1.14.2',
+        'pathos>=0.2.3',
+        'sklearn>=0.19.0',
+        'scipy>=1.0.1',
+        ],
+    scripts = ['straglr.py',
+               ],
+)
