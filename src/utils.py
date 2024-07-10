9c0c0500fc129b98ec1d7000e6934fa358869730
Readman Chiu <rchiu@bcgsc.ca>
2024-07-10 09:50:30 -0700

    move is_same_repeat() here so that variant.py can use

--- src/utils.py
+++ src/utils.py
@@ -69,3 +69,54 @@ def complement_spans(spans):
     for i in range(1, len(spans_merged)):
         complement.append([spans[i-1][1] + 1, spans[i][0] - 1])
     return complement
+
+def is_same_repeat(reps, same_pats=None, min_fraction=0.5):
+        def check_same_pats(rep1, rep2):
+            if rep1 in same_pats:
+                if rep2 in same_pats[rep1]:
+                    return True
+
+                for rep in same_pats[rep1]:
+                    if rep2 in rep:
+                        if float(rep.count(rep2) * len(rep2)) / len(rep) >= min_fraction:
+                            return True
+                    elif rep in rep2:
+                        if float(rep2.count(rep) * len(rep)) / len(rep2) >= min_fraction:
+                            return True
+
+            return False
+
+        if len(reps[0]) <= len(reps[1]):
+            rep1, rep2 = reps[0], reps[1]
+        else:
+            rep2, rep1 = reps[0], reps[1]
+
+        if rep1 == rep2:
+            return True
+
+        perms1 = []
+        for i in range(len(rep1)):
+            pat = rep1[i:] + rep1[:i]
+            perms1.append(pat)
+
+        for p1 in perms1:
+            if p1 in rep2:
+                if float(rep2.count(p1) * len(p1)) / len(rep2) >= min_fraction:
+                    return True
+
+        if same_pats:
+            if check_same_pats(reps[0], reps[1]) or check_same_pats(reps[1], reps[0]):
+                return True
+
+            for i in range(0, len(reps[0]), 5):
+                pat = reps[0][i:] + reps[0][:i]
+                if check_same_pats(pat, reps[1]) or check_same_pats(reps[1], pat):
+                    return True
+
+            for i in range(0, len(reps[1]), 5):
+                pat = reps[1][i:] + reps[1][:i]
+                if check_same_pats(pat, reps[0]) or check_same_pats(reps[0], pat):
+                    return True
+
+        return False
+
