d5180cd0d79afef95af5d5ab4da5dabeb63e9959
Readman Chiu <rchiu@bcgsc.ca>
2024-09-18 10:38:01 -0700

    group parameters

--- straglr.py
+++ straglr.py
@@ -13,30 +13,40 @@ def parse_args():
     parser.add_argument("bam", type=str, help="bam file")
     parser.add_argument("genome_fasta", type=str, help="genome_fasta")
     parser.add_argument("out_prefix", type=str, help="output prefix")
-    parser.add_argument("--reads_fasta", type=str, nargs='+', help="read indexed fasta file(s)")
-    parser.add_argument("--min_ins_size", type=int, default=100, help="minimum insertion size. Default:100")
-    parser.add_argument("--exclude", type=str, help="bed file to exclude regions")
-    parser.add_argument("--regions", type=str, help="bed file for scanning only specific regions")
-    parser.add_argument("--nprocs", type=int, help="number of processes", default=1)
-    parser.add_argument("--chroms", type=str, nargs="+", help="chromosomes")
-    parser.add_argument("--loci", type=str, help="bed file of loci for genotyping")
-    parser.add_argument("--sample", type=str, help="sample name for VCF output", default='.')
-    parser.add_argument("--sex", type=str, help="sex, m(ale) or f(emale)", default='f')
-    parser.add_argument("--include_alt_chroms", action='store_true', help="include alternate chromosomes. By default, only chroms 1-22,X,Y are considered in genome scan")
-    parser.add_argument("--min_support", type=int, help="minimum number of supporting reads for detecting expansion. Default:2", default=2)
-    parser.add_argument("--min_cluster_size", type=int, help="minimum number of supporting reads for allele clustering. Default:2", default=2)
-    parser.add_argument("--genotype_in_size", action="store_true", help="report genotype in size instead of copy numbers")
-    parser.add_argument("--max_str_len", type=int, help="maximum STR length. Default:50", default=50)
-    parser.add_argument("--min_str_len", type=int, help="minimum STR length. Default:2", default=2)
-    parser.add_argument("--max_num_clusters", type=int, help="maximum number of clusters to try. Default:2", default=2)
-    parser.add_argument("--max_cov", type=int, help="maximum allowed coverage for ins inspection. Default:100", default=100)
-    parser.add_argument("--use_unpaired_clips", action='store_true', help="also examine unpaired clipped alignments in genome scan")
-    parser.add_argument("--include_partials", action='store_true', help="detect and report reads only capturing partial repeats when genotyping")
-    parser.add_argument("--trf_args", type=int, nargs=7, help="tandem repeat finder arguments. Default:2 5 5 80 10 10 500", metavar=trf_args_meta, default=[2,5,5,80,10,10,500])
-    parser.add_argument("--symbolic", action='store_true', help="output symbolic alleles instead of actual sequences for ALTs in VCF output")
-    parser.add_argument("--tmpdir", type=str, help="directory to use for generating tmp files instead of system TEMP")
-    parser.add_argument("--debug", action='store_true', help="debug mode i.e. keep trf output")
-    parser.add_argument("--version", action='version', version=__version__)
+
+    scan = parser.add_argument_group('genome scan')
+    scan.add_argument("--min_ins_size", type=int, default=100, help="minimum insertion size. Default:100")
+    scan.add_argument("--min_str_len", type=int, help="minimum STR length. Default:2", default=2)
+    scan.add_argument("--max_str_len", type=int, help="maximum STR length. Default:50", default=50)
+    scan.add_argument("--min_support", type=int, help="minimum number of supporting reads for detecting expansion. Default:2", default=2)
+    scan.add_argument("--trf_args", type=int, nargs=7, help="tandem repeat finder arguments. Default:2 5 5 80 10 10 500", metavar=trf_args_meta, default=[2,5,5,80,10,10,500])
+    scan.add_argument("--chroms", type=str, nargs="+", help="chromosomes")
+    scan.add_argument("--regions", type=str, help="bed file for scanning only specific regions")
+    scan.add_argument("--exclude", type=str, help="bed file to exclude regions")
+    scan.add_argument("--include_alt_chroms", action='store_true', help="include alternate chromosomes. By default, only chroms 1-22,X,Y are considered in genome scan")
+    scan.add_argument("--max_cov", type=int, help="maximum allowed coverage for ins inspection. Default:100", default=100)
+    scan.add_argument("--use_unpaired_clips", action='store_true', help="also examine unpaired clipped alignments in genome scan")
+
+    genotyping = parser.add_argument_group('genotyping')
+    genotyping.add_argument("--loci", type=str, help="bed file of loci for genotyping")
+    genotyping.add_argument("--genotype_in_size", action="store_true", help="report genotype in size instead of copy numbers")
+    genotyping.add_argument("--include_partials", action='store_true', help="detect and report reads only capturing partial repeats when genotyping")
+
+    io = parser.add_argument_group('input/output')
+    io.add_argument("--sample", type=str, help="sample name for VCF output", default='.')
+    io.add_argument("--sex", type=str, help="sex, m(ale) or f(emale)", default='f')
+    io.add_argument("--symbolic", action='store_true', help="output symbolic alleles instead of actual sequences for ALTs in VCF output")
+    io.add_argument("--reads_fasta", type=str, nargs='+', help="read indexed fasta file(s)")
+    io.add_argument("--nprocs", type=int, help="number of processes", default=1)
+    io.add_argument("--tmpdir", type=str, help="directory to use for generating tmp files instead of system TEMP")
+    io.add_argument("--debug", action='store_true', help="debug mode i.e. keep trf output")
+    io.add_argument("--version", action='version', version=__version__)
+
+    clustering = parser.add_argument_group('clustering (for defining alleles)')
+    clustering.add_argument("--min_cluster_size", type=float, help="minimum number or fraction of supporting reads for allele clustering. Default:0.1", default=0.1)
+    clustering.add_argument("--max_num_clusters", type=int, help="maximum number of clusters to try. Default:2", default=2)
+    clustering.add_argument("--max_check_size", type=int, help="threshold of cluster (allele) size (median/mean) to skip filtering by --min_cluster_size. Default:5000", default=5000)
+
     args = parser.parse_args()
     return args
 
@@ -46,7 +56,7 @@ def main():
     if args.tmpdir:
         tempfile.tempdir = args.tmpdir
 
-    min_cluster_size = args.min_cluster_size if args.min_cluster_size < args.min_support else args.min_support
+    #min_cluster_size = args.min_cluster_size if args.min_cluster_size < args.min_support else args.min_support
 
     sex = args.sex[0].lower()
     if sex != 'm':
@@ -58,7 +68,8 @@ def main():
                            max_str_len=args.max_str_len,
                            min_str_len=args.min_str_len,
                            min_support=args.min_support,
-                           min_cluster_size=min_cluster_size,
+                           min_cluster_size=args.min_cluster_size,
+                           max_check_size=args.max_check_size,
                            genotype_in_size=args.genotype_in_size,
                            max_num_clusters=args.max_num_clusters,
                            trf_args=' '.join(map(str, args.trf_args + ['-d', '-h'])),
